c8a45957bc38c7dcc9dc0a0f78ff32f771128544
Morgan Laversin <morganlaversin@gmail.com>
2022-11-23 21:25:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  console.log('browser ok');
+
+  const page = await browser.newPage();
+  console.log('page ok');
+
+  await (await page).goto('https://fr.wikipedia.org/wiki/Node.js');
+  console.log('website ok');
+
+  await page.setViewport({
+    width: 1200,
+    height: 800,
+  });
+  console.log('size ok');
+
+  //   await page.screenshot({
+  //     path: 'screen.png',
+  //   });
+  //   console.log('screen ok');
+
+  await page.pdf({
+    path: 'screen.pdf',
+    format: 'A4',
+  });
+})().catch(err => console.log(err.message));
